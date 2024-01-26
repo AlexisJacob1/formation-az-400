85278fb69e975160b3791c49c1da6a47b92ae12e
Alexis Jacob <alexis.jacob@sincro.fr>
2024-01-26 15:12:16 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace formation_devops.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
